e2f25fa45a99c9915251d884420ccb58cdb10534
Marcos Manuel Ortega <info@indavelopers.com>
2024-04-16 20:34:40 +0200

    Config in pulumi.stack.yaml

--- __main__.py
+++ __main__.py
@@ -3,51 +3,46 @@
 import pulumi
 import pulumi_gcp as gcp
 
-# Config
-# todo: get configs from config file
-N_STUDENTS = 3
-ORGANIZATION_ID = "852525814022"
-BILLING_ACCOUNT_ID = "00AF3E-CE59B2-0F49AE"
-FOLDER_NAME = 'training-labs'
-FOLDER_DISPLAY_NAME = 'Training Labs'
-# E.g. Project IDs created: 'PROJECT_PREFIX-0-PROJECT_RANDOM_SUFFIX'
-PROJECT_PREFIX = 'student'
-PROJECT_RANDOM_SUFFIX = 'qwerty'
 
-# It's recommended to create n_students + 1 projects, having "project-0" for the instructor
-project_names = [PROJECT_PREFIX + '-' + str(i) for i in range(N_STUDENTS)]
+# Config
+config = pulumi.Config()
+organization_id = config.require('organization_id')
+billing_account_id = config.require('billing_account_id')
+folder_name  = config.require('folder_name')
+folder_display_name = config.require('folder_display_name')
+project_prefix = config.require('project_prefix')
+project_random_suffix = config.require('project_random_suffix')
+emails = config.require_object('emails')
+roles = config.require_object('roles')
+apis = config.require_object('apis')
+n_students = len(emails)
+
+
+# It's recommended to create n_students + 1 projects, having "project_prefix-0-suffix" for the instructor
+project_names = [project_prefix + '-' + str(i) for i in range(n_students)]
 
 # Create a GCP folder for the training projects
-folder = gcp.organizations.Folder(FOLDER_NAME, display_name=FOLDER_DISPLAY_NAME, parent=f'organizations/{ORGANIZATION_ID}')
+folder = gcp.organizations.Folder(folder_name, display_name=folder_display_name, parent=f'organizations/{organization_id}')
 
 # Create GCP projects under said folder
 project_ids = []
 for project_name in project_names:
-    project_id = project_name + '-' + PROJECT_RANDOM_SUFFIX
+    project_id = project_name + '-' + project_random_suffix
     project_ids.append(project_id)
 
-    project = gcp.organizations.Project(project_id, name=project_name, project_id=project_id, folder_id=folder.name, billing_account=BILLING_ACCOUNT_ID)
+    project = gcp.organizations.Project(project_id, name=project_name, project_id=project_id, folder_id=folder.name, billing_account=billing_account_id)
 
-# Exports
-pulumi.export('organization_id', ORGANIZATION_ID)
-pulumi.export('billing_account', BILLING_ACCOUNT_ID)
 pulumi.export('folder_id', folder.name)
 pulumi.export('project_ids', project_ids)
 
 
 # Enable APIs on projects
-apis = ['iam.googleapis.com', 'storage.googleapis.com']
+# todo: enable apis
 
-pulumi.export('enabled_apis', apis)
+pulumi.export('apis', apis)
 
 
 # Setup IAM for the students
-
-# todo: load students emails from file
-emails = ['konigdermasai@gmail.com', 'konigdermasai@gmail.com', 'konigdermasai@gmail.com']
-roles = ['roles/storage.admin']
-
-# todo: check because role is not assigned
 for project_id, email in zip(project_ids, emails):    
     for role in roles:
         project_binding = gcp.projects.IAMMember(project_id, project=project_id, role=role, member=f'user:{email}')
